6dfd33f565a9fbe9b3ab7eedc82196de4d801b58
nayeem-17 <nasa.28apr@gmail.com>
2023-07-21 09:08:08 +0600

    init commit

---
+++ src/main/java/com/example/codelytic/CodelyticApplication.java
@@ -0,0 +1,13 @@
+package com.example.codelytic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodelyticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodelyticApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/codelytic/CodelyticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codelytic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodelyticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
